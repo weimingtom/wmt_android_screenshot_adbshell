8d96cc88df189d3c00d54243cfb2b7c4c0566774
weimingtom <weimingtom@qq.com>
2016-11-11 22:41:12 +0800

    first commit

---
+++ mycap/jni/Android.mk
@@ -0,0 +1,113 @@
+LOCAL_PATH := $(call my-dir)
+
+#===================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gui
+
+LOCAL_SRC_FILES := fake_libgui.cpp
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) 
+
+LOCAL_LDLIBS := -lGLESv2 -llog -lz
+LOCAL_EXPORT_LDLIBS := -lGLESv2 -llog -lz
+
+#LOCAL_WHOLE_STATIC_LIBRARIES := 
+
+LOCAL_CFLAGS := 
+LOCAL_CFLAGS += -fPIE
+LOCAL_LDFLAGS += -fPIE
+#LOCAL_EXPORT_CFLAGS := 
+
+include $(BUILD_SHARED_LIBRARY)
+#===================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := binder
+
+LOCAL_SRC_FILES := fake_libbinder.cpp
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) 
+
+LOCAL_LDLIBS := -lGLESv2 -llog -lz
+LOCAL_EXPORT_LDLIBS := -lGLESv2 -llog -lz
+
+#LOCAL_WHOLE_STATIC_LIBRARIES := 
+
+LOCAL_CFLAGS := 
+LOCAL_CFLAGS += -fPIE
+LOCAL_LDFLAGS += -fPIE
+#LOCAL_EXPORT_CFLAGS := 
+
+include $(BUILD_SHARED_LIBRARY)
+#===================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mycap
+
+LOCAL_SRC_FILES := mycap.cpp
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) 
+
+LOCAL_LDLIBS := -lGLESv2 -llog -lz
+LOCAL_EXPORT_LDLIBS := -lGLESv2 -llog -lz
+
+#LOCAL_WHOLE_STATIC_LIBRARIES := 
+
+LOCAL_CFLAGS := 
+LOCAL_CFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+LOCAL_LDFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+
+#LOCAL_EXPORT_CFLAGS := 
+
+include $(BUILD_EXECUTABLE)
+#===================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mycap-ics
+
+LOCAL_SRC_FILES := mycap.cpp
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) 
+
+LOCAL_LDLIBS := -lGLESv2 -llog -lz
+LOCAL_EXPORT_LDLIBS := -lGLESv2 -llog -lz
+
+#LOCAL_WHOLE_STATIC_LIBRARIES := 
+
+LOCAL_CFLAGS := -DTARGET_ICS
+LOCAL_CFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+LOCAL_LDFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+
+#LOCAL_EXPORT_CFLAGS := 
+LOCAL_SHARED_LIBRARIES := gui binder
+
+include $(BUILD_EXECUTABLE)
+#===================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mycap-jb
+
+LOCAL_SRC_FILES := mycap.cpp
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) 
+LOCAL_C_INCLUDES := $(LOCAL_PATH) 
+
+LOCAL_LDLIBS := -lGLESv2 -llog -lz
+LOCAL_EXPORT_LDLIBS := -lGLESv2 -llog -lz
+
+#LOCAL_WHOLE_STATIC_LIBRARIES := 
+
+LOCAL_CFLAGS := -DTARGET_JB
+LOCAL_CFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+LOCAL_LDFLAGS += -pie -fPIE -Xlinker -rpath=/system/lib
+
+#LOCAL_EXPORT_CFLAGS := 
+LOCAL_SHARED_LIBRARIES := gui binder
+
+include $(BUILD_EXECUTABLE)
+#===================================
---
+++ mycap/readme.txt
@@ -0,0 +1,8 @@
+https://github.com/zlcnup/android-screen-capture
+https://github.com/buptpatriot/2048-AI
+https://github.com/bopo/sji-android-screen-capture
+
+> adb push mycap-jb /data/local/tmp
+$ cd /data/local/tmp
+$ chmod 744 mycap-jb
+$ ./mycap-jb 1 > a.bmp
